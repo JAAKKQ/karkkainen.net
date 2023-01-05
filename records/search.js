555aa70504a0fe688122503f3b8e1e412a93f31e
Jaakko <mail@r3ne.net>
2023-01-05 22:57:34 +0200

    Spin

--- records/search.js
+++ records/search.js
@@ -55,8 +55,6 @@ function displayResults(results) {
   });
 }
 
-
-
 const searchForm = document.getElementById('search-form');
 searchForm.addEventListener('submit', event => {
   event.preventDefault();
