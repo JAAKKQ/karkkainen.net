ebd828de6864d138fdd5e6516cfb89aea12d801b
Jaakko <mail@r3ne.net>
2023-01-05 21:15:19 +0200

    Update search.js

--- records/search.js
+++ records/search.js
@@ -20,34 +20,34 @@ function searchRecords(query) {
 function displayResults(results) {
     const resultsContainer = document.getElementById('results');
     resultsContainer.innerHTML = '';
-    const numResults = results.length;
-    const numResultsText = `${numResults} result(s)`;
-    resultsContainer.insertAdjacentHTML('beforeend', `<p>${numResultsText}</p>`);
     results.forEach(result => {
-        const resultElement = document.createElement('div', 'class="cover"');
+        const resultElement = document.createElement('div');
+        resultElement.style.clear = 'both';
         resultElement.innerHTML = `
-        <img src="${result.result.cover_image}" alt="${result.result.title} cover">
-        <h2>${result.result.title}</h2>
-        <p>${result.result.country} (${result.result.year})</p>
-        <p>Genre: ${result.result.genre.join(', ')}</p>
-        <p>Style: ${result.result.style.join(', ')}</p>
-        <p>Format: ${result.result.format.join(', ')}</p>
-        <p>Label: ${result.result.label.join(', ')}</p>
-        <p>PRICES:</p>
-        <ul>
-        ${Object.entries(result.price).map(([key, value]) => {
-            if (value && value.value) {
+        <img src="${result.result.cover_image}" style="float: left; width: 30%; margin-right: 10px;">
+        <div style="float: left; width: 70%;">
+          <h2 style="margin-bottom: 10px;">${result.result.title}</h2>
+          <p style="margin-bottom: 10px;">${result.result.country} (${result.result.year})</p>
+          <p style="margin-bottom: 10px;">Genre: ${result.result.genre.join(', ')}</p>
+          <p style="margin-bottom: 10px;">Style: ${result.result.style.join(', ')}</p>
+          <p style="margin-bottom: 10px;">Format: ${result.result.format.join(', ')}</p>
+          <p style="margin-bottom: 10px;">Label: ${result.result.label.join(', ')}</p>
+          <p>PRICES:</p>
+          <ul>
+            ${Object.entries(result.price).map(([key, value]) => {
+              if (value && value.value) {
                 return `<li>${key}: ${value.value.toFixed(2)} ${value.currency}</li>`
-            } else {
+              } else {
                 return `<li>${key}: N/A</li>`
-            }
-        }).join('')}
-          
-        </ul>
+              }
+            }).join('')}
+          </ul>
+        </div>
       `;
         resultsContainer.appendChild(resultElement);
-    });    
+    });
 }
+
 
 
 const searchForm = document.getElementById('search-form');
