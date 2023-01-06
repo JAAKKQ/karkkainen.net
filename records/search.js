dd94a1c02a857a25e735ef6191b468fd2d118a56
Jaakko <mail@r3ne.net>
2023-01-07 01:56:53 +0200

    Update search.js

--- records/search.js
+++ records/search.js
@@ -37,9 +37,8 @@ function displayResults(results) {
     resultElement.style.clear = "both";
     resultElement.innerHTML = `
         <img src="${result.result.cover_image}" style="float: left; width: 100%; margin-right: 10px;">
-        ${
-          result.songs
-            ? `        <div style="float: left; width: 70%;">
+        ${result.songs
+        ? ` <div style="float: left; width: 70%;">
             <h2 style="margin-bottom: 10px;">${result.result.title}</h2>
             <p style="margin-bottom: 10px;">${result.result.country} (${result.result.year})</p>
             <p style="margin-bottom: 10px;">Genre: ${result.result.genre.join(", ")}</p>
@@ -47,7 +46,7 @@ function displayResults(results) {
             <p style="margin-bottom: 10px;">Format: ${result.result.format.join(", ")}</p>
             <p style="margin-bottom: 10px;">Label: ${result.result.label.join(", ")}</p>
             <h3>Prices</h3>
-            <ul>
+            <ul style="padding-top: -100px;">
               ${Object.entries(result.price)
           .map(
             ([key, value]) =>
@@ -58,13 +57,13 @@ function displayResults(results) {
           .join("")}
             </ul>
           </div><ol>${result.songs
-                .map(
-                  (song) =>
-                    `<li>${song.position ? `${song.position}: ` : ""}${song.title}</li>`
-                )
-                .join("")}</ol>`
-            : ""
-        }
+          .map(
+            (song) =>
+              `<li>${song.position ? `${song.position}: ` : ""}${song.title}</li>`
+          )
+          .join("")}</ol>`
+        : ""
+      }
       `;
     resultsContainer.appendChild(resultElement);
   });
