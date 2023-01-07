0353966cac80b799f062395cdfc6652c0e407c79
Jaakko <mail@r3ne.net>
2023-01-07 02:02:43 +0200

    R

--- records/search.js
+++ records/search.js
@@ -36,38 +36,36 @@ function displayResults(results) {
     const resultElement = document.createElement("div");
     resultElement.style.clear = "both";
     resultElement.innerHTML = `
-    <img src="${result.result.cover_image}" style="float: left; width: 100%; margin-right: 10px;">
-    <div style="float: left; width: 80%;">
-      <h2 style="margin-bottom: 10px;">${result.result.title}</h2>
-      <p style="margin-bottom: 10px;">${result.result.country} (${result.result.year})</p>
-      <p style="margin-bottom: 10px;">Genre: ${result.result.genre.join(", ")}</p>
-      <p style="margin-bottom: 10px;">Style: ${result.result.style.join(", ")}</p>
-      <p style="margin-bottom: 10px;">Format: ${result.result.format.join(", ")}</p>
-      <p style="margin-bottom: 10px;">Label: ${result.result.label.join(", ")}</p>
-      ${result.songs
-        ? `<p style="margin-bottom: 10px;">Songs:</p><ol style="float: left; width: 30%;">${result.songs
-          .map(
-            (song) =>
-              `<li>${song.position ? `${song.position}: ` : ""}${song.title}</li>`
-          )
-          .join("")}</ol>`
-        : ""
-      }
-    </div>
-    <div style="float: right; width: 30%;">
-      <p>PRICES:</p>
-      <ul>
-        ${Object.entries(result.price)
-        .map(
-          ([key, value]) =>
-            value && value.value
-              ? `<li>${key}: ${Number(value.value).toFixed(2)} ${value.currency}</li>`
-              : `<li>${key}: N/A</li>`
-        )
-        .join("")}
-      </ul>
-    </div>
-  `;
+        <img src="${result.result.cover_image}" style="float: left; width: 100%; margin-right: 10px;">
+        <div style="float: left; width: 70%;">
+          <h2 style="margin-bottom: 10px;">${result.result.title}</h2>
+          <p style="margin-bottom: 10px;">${result.result.country} (${result.result.year})</p>
+          <p style="margin-bottom: 10px;">Genre: ${result.result.genre.join(", ")}</p>
+          <p style="margin-bottom: 10px;">Style: ${result.result.style.join(", ")}</p>
+          <p style="margin-bottom: 10px;">Format: ${result.result.format.join(", ")}</p>
+          <p style="margin-bottom: 10px;">Label: ${result.result.label.join(", ")}</p>
+          <p>Songs:</p>
+          <ol>
+            ${result.songs
+              .map((song) => `<li>${song.title}</li>`)
+              .join("")}
+          </ol>
+          <p>PRICES:</p>
+          <ul>
+            ${Object.entries(result.price)
+              .map(([key, value]) => {
+                if (value && value.value) {
+                  return `<li>${key}: ${Number(value.value).toFixed(
+                    2
+                  )} ${value.currency}</li>`;
+                } else {
+                  return `<li>${key}: N/A</li>`;
+                }
+              })
+              .join("")}
+          </ul>
+        </div>
+      `;
     resultsContainer.appendChild(resultElement);
   });
 }
