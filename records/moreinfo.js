618ad0cb1ba9974490f1c0c8ea85e7a7cb395a21
Jaakko <mail@r3ne.net>
2023-01-14 21:41:19 +0200

    More info

--- records/moreinfo.js
+++ records/moreinfo.js
@@ -7,12 +7,27 @@ fetch("../records.json")
         }
         const record = records.find((record) => record.result.id == id);
         // rest of the code here
-        document.getElementById("record-title").innerHTML = record.result.title;
-        document.getElementById("record-artist").innerHTML = record.result.artist;
-        document.getElementById("record-year").innerHTML = record.result.year;
-        document.getElementById("record-genre").innerHTML = record.result.genre;
-        document.getElementById("record-format").innerHTML = record.result.format;
-        document.getElementById("record-label").innerHTML = record.result.label;
+        resultElement.innerHTML = `
+        <img src="${result.result.cover_image}" style="float: left; width: 100%; margin-top: 15px; margin-right: 10px;">
+        <div style="float: left; width: 70%;">
+          <h2 style="margin-bottom: 10px;"><a href="moreinfo.html?id=${result.result.id}" target="_blank" id="title-link">${result.result.title}</a></h2>
+          <p style="margin-bottom: 10px;">${result.result.country} (${result.result.year})</p>
+          <p style="margin-bottom: 10px;">Genre: ${result.result.genre.join(', ')}</p>
+          <p style="margin-bottom: 10px;">Style: ${result.result.style.join(', ')}</p>
+          <p style="margin-bottom: 10px;">Format: ${result.result.format.join(', ')}</p>
+          <p style="margin-bottom: 10px;">Label: ${result.result.label.join(', ')}</p>
+          <p>PRICES:</p>
+          <ul>
+            ${Object.entries(result.price).map(([key, value]) => {
+            if (value && value.value) {
+                return `<li>${key}: ${Number(value.value).toFixed(2)} ${value.currency}</li>`
+            } else {
+                return `<li>${key}: N/A</li>`
+            }
+        }).join('')}
+          </ul>
+        </div>
+      `;
     })
     .catch((error) => {
         console.log(error);
