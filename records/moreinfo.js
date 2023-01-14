8df19e1eceadf23b2b04e5635c2bf308ef3d6c7a
Jaakko <mail@r3ne.net>
2023-01-14 22:06:37 +0200

    Update moreinfo.js

--- records/moreinfo.js
+++ records/moreinfo.js
@@ -7,11 +7,11 @@ fetch("../records.json")
         const record = records.find((record) => record.result.id === parseInt(id));
         console.log(record);
 
-        document.getElementById("record-title").innerHTML = `${result.result.country} (${result.result.year})`;
-        document.getElementById("record-year").innerHTML = `Genre: ${result.result.genre.join(', ')}`;
-        document.getElementById("record-genre").innerHTML = `Style: ${result.result.style.join(', ')}`;
-        document.getElementById("record-format").innerHTML = `Format: ${result.result.format.join(', ')}`;
-        document.getElementById("record-label").innerHTML = `Label: ${result.result.label.join(', ')}`;
+        document.getElementById("record-title").innerHTML = `${record.result.country} (${record.result.year})`;
+        document.getElementById("record-year").innerHTML = `Genre: ${record.result.genre.join(', ')}`;
+        document.getElementById("record-genre").innerHTML = `Style: ${record.result.style.join(', ')}`;
+        document.getElementById("record-format").innerHTML = `Format: ${record.result.format.join(', ')}`;
+        document.getElementById("record-label").innerHTML = `Label: ${record.result.label.join(', ')}`;
         let songsHTML = '';
         for (let song of record.songs) {
             const { title, duration } = song;
