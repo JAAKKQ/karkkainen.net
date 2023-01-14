108b1ecc8b9cf25c591e10ba336b4be07454c110
Jaakko <mail@r3ne.net>
2023-01-14 21:49:07 +0200

    Update moreinfo.js

--- records/moreinfo.js
+++ records/moreinfo.js
@@ -3,21 +3,19 @@ fetch("../records.json")
     .then((records) => {
         const urlParams = new URLSearchParams(window.location.search);
         const id = urlParams.get('id');
+
         const record = records.find((record) => record.result.id === parseInt(id));
+        console.log(record);
+
         document.getElementById("record-title").innerHTML = record.result.title;
         document.getElementById("record-artist").innerHTML = record.result.artist;
         document.getElementById("record-year").innerHTML = record.result.year;
         document.getElementById("record-genre").innerHTML = record.result.genre;
         document.getElementById("record-format").innerHTML = record.result.format;
         document.getElementById("record-label").innerHTML = record.result.label;
-        // Display the songs
-        const songsContainer = document.getElementById("record-songs");
-        record.result.songs.forEach((song) => {
-            const songElement = document.createElement('li');
-            songElement.innerHTML = song.title;
-            songsContainer.appendChild(songElement);
-        });
+        document.getElementById("record-songs").innerHTML = record.result.songs.join(', ');
     });
+
 
 
 const backButton = document.getElementById("back-button");
