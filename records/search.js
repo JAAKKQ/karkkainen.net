22624990373d195529ea0d2755cf0968eb7e8723
Jaakko <mail@r3ne.net>
2023-03-11 18:57:32 +0200

    Update search.js

--- records/search.js
+++ records/search.js
@@ -70,30 +70,32 @@ function displayResults(results) {
       window.location.href = titleLink.href;
     });
   });
-  const images = document.querySelectorAll('img[data-src]');
-  const config = {
-    rootMargin: '0px 0px 50px 0px',
-    threshold: 0
-  };
-  let observer = new IntersectionObserver(function (entries, self) {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        let lazyImage = entry.target;
-        lazyImage.src = lazyImage.dataset.src;
-        observer.unobserve(lazyImage);
-      }
-    });
-  }, config);
-  images.forEach(image => {
-    observer.observe(image);
-  });
-  // Disable lazy loading of images when user scrolls
-  let image1s = resultsContainer.querySelectorAll('img[loading="lazy"]');
-  window.addEventListener('scroll', () => {
-    images1.forEach((img) => {
-      img.removeAttribute('loading');
-    });
-  });
+
+  var timer = null;
+  window.addEventListener('scroll', function () {
+    if (timer !== null) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(function () {
+      const images = document.querySelectorAll('img[data-src]');
+      const config = {
+        rootMargin: '0px 0px 50px 0px',
+        threshold: 0
+      };
+      let observer = new IntersectionObserver(function (entries, self) {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            observer.unobserve(lazyImage);
+          }
+        });
+      }, config);
+      images.forEach(image => {
+        observer.observe(image);
+      });
+    }, 150);
+  }, false);
 }
 
 const searchForm = document.getElementById('search-form');
