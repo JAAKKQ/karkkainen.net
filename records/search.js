4fdba5076c4efbe34ee79adc79a47396df26f302
R3ne <mail@r3ne.net>
2023-04-25 20:09:12 +0300

    Update search.js

--- records/search.js
+++ records/search.js
@@ -1,11 +1,28 @@
 let records = [];
 
-fetch("../records.json")
-  .then((response) => response.json())
-  .then((recordsobj) => {
-    records = recordsobj;
+// Create a new XHR object
+var xhr = new XMLHttpRequest();
+
+// Open a new connection to the JSON file
+xhr.open('GET', '../records.json', true);
+
+// Set the response type to JSON
+xhr.responseType = 'json';
+
+// When the XHR object receives a response, execute this function
+xhr.onload = function() {
+  // Check if the response status is OK (200)
+  if (xhr.status === 200) {
+    // Access the JSON data using the response property
+    var records = xhr.response;
+    // Do something with the JSON data
     searchRecords("");
-  });
+  }
+};
+
+// Send the request to the server
+xhr.send();
+
 
 function searchObject(obj, query) {
   let matches = false;
