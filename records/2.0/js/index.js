e1f8f70fe1f83fcf8cde1f349823704144ef5ec2
R3ne <mail@r3ne.net>
2023-07-23 21:26:39 +0300

    Update index.js

--- records/2.0/js/index.js
+++ records/2.0/js/index.js
@@ -4,21 +4,97 @@ let records = [];
 var xhr = new XMLHttpRequest();
 
 // Open a new connection to the JSON file
-xhr.open('GET', '../../records.json', true);
+xhr.open('GET', '../../../records.json', true);
 
 // Set the response type to JSON
 xhr.responseType = 'json';
 
 // When the XHR object receives a response, execute this function
-xhr.onload = function() {
-  // Check if the response status is OK (200)
-  if (xhr.status === 200) {
-    // Access the JSON data using the response property
-    records = xhr.response;
-    // Do something with the JSON data
-    searchRecords("");
-  }
+xhr.onload = function () {
+    // Check if the response status is OK (200)
+    if (xhr.status === 200) {
+        // Access the JSON data using the response property
+        records = xhr.response;
+        // Do something with the JSON data
+        searchRecords("");
+    }
 };
 
 // Send the request to the server
 xhr.send();
+
+function searchObject(obj, query) {
+    let matches = false;
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const value = obj[key];
+            if (typeof value === 'string') {
+                matches = value.toLowerCase().includes(query);
+            } else if (typeof value === 'number') {
+                matches = value.toString().includes(query);
+            } else if (Array.isArray(value)) {
+                matches = value.some((element) => {
+                    if (typeof element === 'string') {
+                        return element.toLowerCase().includes(query);
+                    } else if (typeof element === 'object') {
+                        return searchObject(element, query);
+                    }
+                    return false;
+                });
+            } else if (typeof value === 'object') {
+                matches = searchObject(value, query);
+            }
+            if (matches) break;
+        }
+    }
+    return matches;
+}
+
+function searchRecords(query) {
+    if (typeof query !== 'string') {
+        console.error('Search query must be a string');
+        return;
+    }
+    query = query.toLowerCase();
+    const results = records.filter((record) => {
+        return searchObject(record, query);
+    });
+    console.log(results);
+    displayResults(results);
+}
+
+function addRecordToResults(title, artist, imageUrl) {
+    const resultsContainer = document.getElementById("results-container");
+
+    const recordElement = document.createElement("div");
+    recordElement.classList.add("record");
+
+    const recordImage = document.createElement("img");
+    recordImage.src = imageUrl;
+    recordImage.alt = "Vinyl Record";
+
+    const recordInfo = document.createElement("div");
+    recordInfo.classList.add("record-info");
+
+    const recordTitle = document.createElement("div");
+    recordTitle.classList.add("record-title");
+    recordTitle.textContent = title;
+
+    const recordArtist = document.createElement("div");
+    recordArtist.classList.add("record-artist");
+    recordArtist.textContent = artist;
+
+    recordInfo.appendChild(recordTitle);
+    recordInfo.appendChild(recordArtist);
+
+    recordElement.appendChild(recordImage);
+    recordElement.appendChild(recordInfo);
+
+    resultsContainer.appendChild(recordElement);
+}
+
+function displayResults(results) {
+    results.forEach(result => {
+        addRecordToResults(result.result.title, result.result.genre.join(', '), result.result.cover_image)
+      });
+}
