18ff6b65b7e8a2b1fd3b0ea71d7cb069727915df
R3ne <mail@r3ne.net>
2023-09-09 14:50:35 +0300

    Update 3d.js

--- js/3d.js
+++ js/3d.js
@@ -1,24 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const sentences = [
-    "I am a Finnish technology student who enjoys creating games, software, and other cool projects.",
-    "I'm a Finnish technology student who develops games, software, and other exciting projects.",
-    "With my passion for technology as a Finn, I craft games, software, and other awesome projects.",
-  ];
+const sentences = [
+  "I am a Finnish technology student who enjoys creating games, software, and other cool projects.",
+  "I'm a Finnish technology student who develops games, software, and other exciting projects.",
+  "With my passion for technology as a Finn, I craft games, software, and other awesome projects.",
+];
 
-  const typingText = document.getElementById("typing-text");
+const typingText = document.getElementById("typing-text");
 
-  const randomIndex = Math.floor(Math.random() * sentences.length);
-  const randomSentence = sentences[randomIndex];
+const randomIndex = Math.floor(Math.random() * sentences.length);
+const randomSentence = sentences[randomIndex];
 
-  let charIndex = 0;
+let charIndex = 0;
 
-  function type() {
-    if (charIndex < randomSentence.length) {
-      typingText.textContent += randomSentence.charAt(charIndex);
-      charIndex++;
-      setTimeout(type, 35);
-    }
+function type() {
+  if (charIndex < randomSentence.length) {
+    typingText.textContent += randomSentence.charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 35);
   }
+}
 
-  type();
-});
+type();
