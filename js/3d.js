365ad7f345fc38a0de6412de5ff7814cf2605d62
R3ne <mail@r3ne.net>
2023-09-09 14:47:44 +0300

    Random typewriter text

--- js/3d.js
+++ js/3d.js
@@ -1,13 +1,24 @@
-const focusElement = document.querySelector('model-viewer');
-const container = document.getElementById('3d');
+document.addEventListener("DOMContentLoaded", function () {
+  const sentences = [
+    "I am a Finnish technology student who enjoys creating games, software, and other cool projects.",
+    "I'm a Finnish technology student who develops games, software, and other exciting projects.",
+    "With my passion for technology as a Finn, I craft games, software, and other awesome projects.",
+  ];
 
-focusElement.addEventListener('focus', () => {
-  // Disable scrolling when the element is in focus
-  console.log("Viewer in focus")
-  window.onscroll = function () { window.scrollTo(0, 0); };
-});
+  const typingText = document.getElementById("typing-text");
 
-focusElement.addEventListener('blur', () => {
-  // Enable scrolling when the element loses focus
-  container.style.overflow = 'auto';
+  const randomIndex = Math.floor(Math.random() * sentences.length);
+  const randomSentence = sentences[randomIndex];
+
+  let charIndex = 0;
+
+  function type() {
+    if (charIndex < randomSentence.length) {
+      typingText.textContent += randomSentence.charAt(charIndex);
+      charIndex++;
+      setTimeout(type, 35);
+    }
+  }
+
+  type();
 });
