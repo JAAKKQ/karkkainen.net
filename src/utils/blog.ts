a125787ad8bcef4f22b38294456fdf6794c74a7a
Rene <rene@karkkainen.net>
2024-08-10 01:00:18 +0300

    Hidden Projects Feature
    Added a feature to make specified projects only accessible through a direct link to the page.

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -98,7 +98,7 @@ const load = async function (): Promise<Array<Post>> {
 
   const results = (await Promise.all(normalizedPosts))
     .sort((a, b) => b.publishDate.valueOf() - a.publishDate.valueOf())
-    .filter((post) => !post?.hidden);
+    .filter((post) => !post?.draft);
 
   return results;
 };
@@ -121,6 +121,14 @@ export const blogPostsPerPage = APP_BLOG?.postsPerPage;
 
 /** */
 export const fetchPosts = async (): Promise<Array<Post>> => {
+  if (!_posts) {
+    _posts = await load();
+  }
+
+  return _posts.filter((post) => !post?.hidden);
+};
+
+export const fetchAllPosts = async (): Promise<Array<Post>> => {
   if (!_posts) {
     _posts = await load();
   }
@@ -176,7 +184,7 @@ export const getStaticPathsBlogList = async ({ paginate }: { paginate: PaginateF
 /** */
 export const getStaticPathsBlogPost = async () => {
   if (!isBlogEnabled || !isBlogPostRouteEnabled) return [];
-  return (await fetchPosts()).flatMap((post) => ({
+  return (await fetchAllPosts()).flatMap((post) => ({
     params: {
       blog: post.permalink,
     },
